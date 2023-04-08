20b3274ccdf85474c49c9bb6ec7063b62d6f530d
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2023-04-08 10:45:16 +0530

    UI changes

--- src/components/LectCard.js
+++ src/components/LectCard.js
@@ -1,48 +1,58 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { AiOutlineDownload } from "react-icons/ai";
 
-const LectCard = ({ sz, data }) => {
+const LectCard = ({ sz, data, bar }) => {
   return (
-    <div className={`w-full h-full ${sz === "md" ? "p-2 md:p-4" : "p-1"} `}>
-      <div className="relative h-full rounded-sm overflow-hidden">
-        {data && (
-          <Image
-            className="object-cover"
-            src={data.imgsrc}
-            alt={data.title}
-            fill={true}
-          />
-        )}
-        {data && (
-          <div
-            className={`absolute bg-gray-600 ${
-              sz == "sm" && "text-xs"
-            } bottom-2 right-2 text-sm px-2 py-1 rounded-md`}
-          >
-            {data.time}
+    <Link href={data.link} target="_blank">
+      <div className={`w-full h-full ${sz === "md" ? "p-2 md:p-4" : "p-1"} `}>
+        <div className="relative h-full rounded-sm overflow-hidden">
+          {data && (
+            <Image
+              className="object-cover"
+              src={data.imgsrc}
+              alt={data.title}
+              fill={true}
+            />
+          )}
+          {data && (
+            <div
+              className={`absolute bg-gray-600 ${
+                sz == "sm"
+                  ? "text-[6px] md:text-xs px-1 py-[2px] rounded-sm bottom-1 right-1"
+                  : "text-xs md:text-sm  px-2 py-1 rounded-md bottom-2 right-2"
+              }  `}
+            >
+              {data.time}
+            </div>
+          )}
+        </div>
+        {bar && (
+          <div className="bg-slate-600 h-1 my-1 md:my-2 w-full ">
+            <div className={`bg-red-600 w-${bar} h-full`}></div>
           </div>
         )}
+        <div className="flex justify-between">
+          <p
+            className={`${
+              sz === "md"
+                ? "text-[10px] md:text-lg pt-2"
+                : "text-[8px] md:text-base pt-1"
+            } font-semibold `}
+          >
+            {data && `${data.title} | Lect ${data.lect_no} `}
+          </p>
+          <AiOutlineDownload
+            className={`${
+              sz == "md"
+                ? "text-base md:text-xl mt-2"
+                : "text-sm md:text-lg md:mt-1"
+            }`}
+          />
+        </div>
       </div>
-      <div className="flex justify-between">
-        <p
-          className={`${
-            sz === "md"
-              ? "text-[10px] md:text-lg pt-2"
-              : "text-[6px] md:text-base pt-1"
-          } font-semibold `}
-        >
-          {data && `${data.title} | Lect ${data.lect_no} `}
-        </p>
-        <AiOutlineDownload
-          className={`${
-            sz == "md"
-              ? "text-base md:text-xl mt-2"
-              : "text-sm md:text-lg md:mt-1"
-          }`}
-        />
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/Watched.js
+++ src/components/Watched.js
@@ -8,14 +8,14 @@ const Watched = ({ data }) => {
       </h2>
       <div className="flex w-full lg:h-[550px]">
         <div className="w-2/3 h-[200px] sm:h-[250px] md:h-[350px] lg:h-[500px]">
-          <LectCard sz="md" data={data[0]} />
+          <LectCard sz="md" data={data[0]} bar="2/3" />
         </div>
         <div className="w-1/3 py-4 pe-3">
           <div className="h-[42%] mb-9">
-            <LectCard sz="sm" data={data[1]} />
+            <LectCard sz="sm" data={data[1]} bar="1/3" />
           </div>
           <div className="h-[42%]">
-            <LectCard sz="sm" data={data[2]} />
+            <LectCard sz="sm" data={data[2]} bar="full" />
           </div>
         </div>
       </div>
